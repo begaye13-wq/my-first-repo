534032e9bbb171ec06439fe5b19b9da91ee1f02b
begaye13-wq <begaye13@gmail.com>
2026-04-13 16:11:29 -0600

    personalInfo.js

---
+++ personalInfo.js
@@ -0,0 +1,11 @@
+let name = "Lane Russell";
+let age = 39;
+let height = 62;
+let isStudent = true;
+//(
+let nameAsString = String(name);
+let ageAsString = String(age);
+let heightAsString = String(height);
+let isStudentAsString = String(isStudent);
+let message ="Hello, my name is " + name + ". I am " + age + " years old, " + height + " cm tall, and it is " + isStudent + " that I am a learner."
+console.log(message);
